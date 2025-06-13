5f23b562743744022eb97cc672a3b27e80423cca
ChoiJHy <choior12@gmail.com>
2025-06-13 20:22:52 +0900

    wip: 아이템 베이스 클래스 구현중

--- Source/DungeonStation/Item/BaseInteractableActor.cpp
+++ Source/DungeonStation/Item/BaseInteractableActor.cpp
@@ -2,6 +2,9 @@
 
 
 #include "BaseInteractableActor.h"
+#include "Components/StaticMeshComponent.h"
+#include "Components/SphereComponent.h"
+
 
 // Sets default values
 ABaseInteractableActor::ABaseInteractableActor()
@@ -9,6 +12,66 @@ ABaseInteractableActor::ABaseInteractableActor()
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	DefaultSceneRoot = CreateDefaultSubobject<USceneComponent>(TEXT("DefaultSceneRoot"));
+	RootComponent = DefaultSceneRoot;
+
+	ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("ItemMesh"));
+	ItemMesh->SetupAttachment(RootComponent);
+	ItemMesh->SetCollisionResponseToChannel(ECC_Visibility, ECollisionResponse::ECR_Block);
+
+	// 인식 범위
+	DetectionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("DetectionSphere"));
+	DetectionSphere->SetupAttachment(RootComponent);
+	DetectionSphere->SetSphereRadius(1000.0f);
+	DetectionSphere->SetCollisionProfileName(TEXT("Trigger"));
+
+	// 상호작용 범위
+	InteractionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("InteractionSphere"));
+	InteractionSphere->SetupAttachment(RootComponent);
+	InteractionSphere->SetSphereRadius(200.0f);
+	InteractionSphere->SetCollisionProfileName(TEXT("Trigger"));
+
+	// 기본값 설정
+	InteractionText = FText::FromString(TEXT("상호작용"));
+	bIsPlayerInDetectionRange = false;
+	bIsPlayerInInteractionRange = false;
+}
+
+void ABaseInteractableActor::OnDetectionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweeepResult)
+{
+}
+
+void ABaseInteractableActor::OnDetectionSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void ABaseInteractableActor::OnInteractionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+}
+
+void ABaseInteractableActor::OnInteractionSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex)
+{
+}
+
+void ABaseInteractableActor::UpdateInteractionUI()
+{
+}
+
+void ABaseInteractableActor::OnBeginFocus_Implementation()
+{
+}
+
+void ABaseInteractableActor::OnEndFocus_Implementation()
+{
+}
+
+FText ABaseInteractableActor::GetInteractionText_Implementation()
+{
+	return FText();
+}
+
+void ABaseInteractableActor::Interact_Implementation()
+{
 }
 
 // Called when the game starts or when spawned
--- Source/DungeonStation/Item/BaseInteractableActor.h
+++ Source/DungeonStation/Item/BaseInteractableActor.h
@@ -7,6 +7,7 @@
 #include "../Interface/InteractableInterface.h"
 #include "BaseInteractableActor.generated.h"
 
+class USphereComponent;
 
 UCLASS()
 class DUNGEONSTATION_API ABaseInteractableActor : public AActor, public IInteractableInterface
@@ -17,6 +18,9 @@ public:
 	// Sets default values for this actor's properties
 	ABaseInteractableActor();
 
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
+	USceneComponent* DefaultSceneRoot;
+
 	// 아이템이 가질 Mesh
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components")
 	UStaticMeshComponent* ItemMesh;
@@ -25,8 +29,38 @@ public:
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Interaction")
 	FText InterationText;
 
+	// 플레이어 인식 범위
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components | Interaction")
+	USphereComponent* DetectionSphere;
+
+	// 상호작용 가능 범위
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Components | Interaction")
+	USphereComponent* InteractionSphere;
+
+protected:
+	// 플레이어 인식 범위 안에 있는지 여부
+	bool bIsPlayerInDetectionRange;
+	// 상호작용 범위 안에 있는지 여부
+	bool bIsPlayerInInteractionRange;
+
+	// 이벤트 함수
+	// DetectionSphere 이벤트
+	UFUNCTION()
+	void OnDetectionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweeepResult);
+	UFUNCTION()
+	void OnDetectionSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	// InteractionSphere 이벤트
+	UFUNCTION()
+	void OnInteractionSphereBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+	UFUNCTION()
+	void OnInteractionSphereEndOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex);
+
+	// UI 헬퍼 함수
+	void UpdateInteractionUI();
 public:
 	// 인터페이스 함수의 기본 함수 정의
+
 	virtual void OnBeginFocus_Implementation() override;
 	virtual void OnEndFocus_Implementation() override;
 	virtual FText GetInteractionText_Implementation() override;
